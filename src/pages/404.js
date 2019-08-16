e7c4773343fce78dd476ea816c72664bad7c5aa4
Jordan Nielson <jordanleenielson@churchofjesuschrist.org>
2019-08-16 13:26:45 -0600

    Reset 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Layout from '../components/Layout'
 
-export default ({ pageContext }) => (
-  <Layout pageContext={pageContext}>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn't exist... the sadness.</p>
-    </div>
-  </Layout>
+export default () => (
+  <div>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn't exist... the sadness.</p>
+  </div>
 )
