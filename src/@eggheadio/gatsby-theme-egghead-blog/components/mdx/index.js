8d3a8730099488b23b545c73b78619c77ec15695
Jordan Nielson <jordanleenielson@churchofjesuschrist.org>
2019-09-16 15:45:30 -0600

    Stop using the subtitle component to get bigger h2's

--- src/@eggheadio/gatsby-theme-egghead-blog/components/mdx/index.js
+++ src/@eggheadio/gatsby-theme-egghead-blog/components/mdx/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 import Title from '@eggheadio/gatsby-theme-egghead-blog/src/components/mdx/Title'
-
-import Subtitle from '@eggheadio/gatsby-theme-egghead-blog/src/components/mdx/Subtitle'
 import Paragraph from '@eggheadio/gatsby-theme-egghead-blog/src/components/mdx/Paragraph'
 import Code from '@eggheadio/gatsby-theme-egghead-blog/src/components/mdx/Code'
 
 export default {
   h1: props => <Title {...props} />,
-  h2: props => <Subtitle {...props} />,
   'li.p': props => <Paragraph style={{ display: 'inline-block' }} {...props} />,
   p: props => <Paragraph {...props} />,
   code: Code,
