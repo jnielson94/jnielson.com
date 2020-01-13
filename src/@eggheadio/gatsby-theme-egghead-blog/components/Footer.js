0d30380a203a443df6eb3bfe3979627c4622ee2f
Jordan Nielson <jordanleenielson@churchofjesuschrist.org>
2020-01-13 15:01:54 -0700

    Add convertkit env data for form

--- src/@eggheadio/gatsby-theme-egghead-blog/components/Footer.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Footer from '@eggheadio/gatsby-theme-egghead-blog/src/components/Footer'
-
-const MyFooter = props => <Footer {...props} noSubscribeForm />
-
-export default MyFooter
