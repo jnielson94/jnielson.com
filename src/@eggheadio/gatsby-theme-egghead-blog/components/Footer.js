d53356e623fb7a9b4c863292646b83865ede86d3
Jordan Nielson <jnielson94@gmail.com>
2020-04-08 10:46:43 -0600

    Update deps and make sure undraw is noted

--- src/@eggheadio/gatsby-theme-egghead-blog/components/Footer.js
+++ src/@eggheadio/gatsby-theme-egghead-blog/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Footer from '@eggheadio/gatsby-theme-egghead-blog/src/components/Footer'
 
-const MyFooter = props => <Footer {...props} noSubscribeForm />
+const MyFooter = (props) => <Footer {...props} noSubscribeForm />
 
 export default MyFooter
