8244752a2fe305682e4f419af1ceffff33d91349
Jordan Nielson <jnielson94@gmail.com>
2020-09-04 13:19:03 -0600

    Take away selection styles, adjust callout

--- src/components/Callout.js
+++ src/components/Callout.js
@@ -1,13 +1,16 @@
 /* @jsx jsx */
 import { jsx } from '@emotion/core'
+import { useTheme } from '../@eggheadio/gatsby-theme-egghead-blog/components/Theming'
 
 // This is Prince's awesome Callout! https://prince.dev/callout
 export default function Callout({ variant = 'info', children }) {
+  const theme = useTheme()
   const variantStyles = {
     info: {
-      borderLeft: '5px solid rgb(119, 32, 115)',
-      backgroundColor: 'hsla(303, 74%, 92%, 0.4)',
+      borderLeft: `5px solid ${theme.colors.primary}`,
+      backgroundColor: theme.colors.gray5,
     },
+    // Currently unused
     danger: {
       borderLeft: '5px solid #f44336',
       backgroundColor: 'rgb(253, 236, 234)',
