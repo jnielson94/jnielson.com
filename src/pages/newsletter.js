6df410076e865403b8999021293d374a9be3b9f5
Jordan Nielson <jnielson94@gmail.com>
2021-10-13 15:06:00 -0400

    remove unused variable

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -4,7 +4,7 @@ import Layout from '@eggheadio/gatsby-theme-egghead-blog/src/components/Layout'
 import Container from '@eggheadio/gatsby-theme-egghead-blog/src/components/Container'
 import SubscribeForm from '../@eggheadio/gatsby-theme-egghead-blog/components/Forms/Subscribe'
 
-const Newsletter = ({ data: { site, allMdx } }) => {
+const Newsletter = ({ data: { site } }) => {
   return (
     <Layout site={site} title="Join Jordan's Newsletter" noSubscribeForm>
       <Container>
